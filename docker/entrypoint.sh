20253beb9c64f30cad6df07a8b64c28b2e0464e2
Rob Pungello <rob@pdfsystems.com>
2024-07-22 10:39:30 -0400

    Revert "Run migrations when bringing up container"
    This reverts commit dc0657ffb5705a662d14d3c9d035155ab64d7450.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 set -e
 
-cd /app
-
 # If this is the first time the container is being run, build the frontend assets
 if [ ! -d "/app/public/build" ]; then
+	cd /app
 	npm run build
 fi
-
-php artisan migrate --force
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
