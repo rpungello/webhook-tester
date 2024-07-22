ff9f4c8a73e15e0973cf2fb2f9d6ee8ea5797da0
Rob Pungello <rob@pdfsystems.com>
2024-07-22 12:42:50 -0400

    Allow websockets to be disabled

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Events\WebhookReceivedEvent;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Symfony\Component\HttpFoundation\Response;
 
 class WebhookController extends Controller
@@ -29,7 +30,9 @@ class WebhookController extends Controller
             ]);
         }
 
-        WebhookReceivedEvent::dispatch($model);
+        if (config('app.websocket')) {
+            WebhookReceivedEvent::dispatch($model);
+        }
 
         return response($project->response_body, $project->response_code, [
             'Content-Type' => $project->response_content_type,
