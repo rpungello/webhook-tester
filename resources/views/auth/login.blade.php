0f4658a5fa78f0271cd87d68d7854699c8a9a5b1
Rob Pungello <rob@pdfsystems.com>
2024-07-23 09:09:18 -0400

    CS fixes for login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,13 +8,15 @@
             <h1 class="font-bold text-xl text-center">{{ config('app.name') }}</h1>
 
             <!-- Email address -->
-            <x-input name="email" type="email" label="{{ __('Email') }}" icon="o-at-symbol" error-field="email" class="min-w-96" value="{{ old('email') }}" tabindex="1" />
+            <x-input name="email" type="email" label="{{ __('Email') }}" icon="o-at-symbol" error-field="email"
+                     class="min-w-96" value="{{ old('email') }}" tabindex="1"/>
 
             <!-- Password -->
-            <x-input name="password" type="password" label="{{ __('Password') }}" icon="o-key" error-field="password" class="min-w-96" tabindex="2" />
+            <x-input name="password" type="password" label="{{ __('Password') }}" icon="o-key" error-field="password"
+                     class="min-w-96" tabindex="2"/>
 
             <!-- Remember me -->
-            <x-checkbox name="remember" label="{{ __('Remember me') }}" class="text-sm" tabindex="3" />
+            <x-checkbox name="remember" label="{{ __('Remember me') }}" class="text-sm" tabindex="3"/>
 
             <!-- Forgot password -->
             <a href="{{ route('password.request') }}" class="text-sm text-primary text-right hover:underline">
@@ -23,7 +25,7 @@
 
             <div class="flex flex-col sm:flex-row w-full space-x-4">
                 <a href="{{ route('register') }}" class="btn btn-default flex-grow">{{ __('Register') }}</a>
-                <x-button type="submit" class="btn btn-primary flex-grow" label="{{ __('Sign In') }}" tabindex="4" />
+                <x-button type="submit" class="btn btn-primary flex-grow" label="{{ __('Sign In') }}" tabindex="4"/>
             </div>
         </form>
     </div>
