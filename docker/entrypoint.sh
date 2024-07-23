42c0afe7e178eb531292f59d51e565e8cc0b5c6f
Rob Pungello <rob@pdfsystems.com>
2024-07-23 16:43:38 -0400

    Run isolated migrations during container launch

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 set -e
 
+cd /app
+
 # If this is the first time the container is being run, build the frontend assets
 if [ ! -d "/app/public/build" ]; then
-	cd /app
 	npm run build
 fi
+
+php artisan migrate --force --isolated
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
